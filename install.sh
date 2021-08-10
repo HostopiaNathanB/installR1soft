2a06445891b632934e9fceee763d6777437a2f66
HostopiaNathanS <80649693+HostopiaNathanS@users.noreply.github.com>
2021-08-10 11:19:46 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,19 @@ cat /etc/yum.repos.d/r1soft.repo
 yum install serverbackup-enterprise-agent -y
 
 yum install -y kernel-headers-$(uname -r)
+
+if [[ $? != "0" ]]
+  then
+  echo "Couldn't install Kernel-headers-$(uname -r) Please continue manually"
+  exit
+fi
+
 yum install -y kernel-devel-$(uname -r)
+if [[ $? != "0" ]]
+  then
+  echo "Couldn't install Kernel-devel-$(uname -r) Please continue manually"
+  exit
+fi
 
 r1soft-setup --get-module
 
